03f8c858f82e8644696b086073798e5b441bb9f1
Starfunx <starfunx@hotmail.fr>
2022-11-18 16:59:34 +0100

    microsecond with cycle count

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,12 @@
 
-#![deny(unsafe_code)]
+// #![deny(unsafe_code)]
 #![no_std]
 #![no_main]
 
-use panic_halt as _;
-// use panic_semihosting as _;
+use core::str::Bytes;
+
+// use panic_halt as _;
+use panic_semihosting as _;
 
 use cortex_m_semihosting::hprintln;
 
@@ -14,8 +16,12 @@ use stm32f1xx_hal as hal;
 use hal::timer::Timer;
 use hal::timer::TimerExt;
 
+use fugit::Duration;
+
 mod stepper_driver;
 use stepper_driver::StepperDriver;
+
+use cortex_m::peripheral::SYST;
 
 #[entry]
 fn main() -> ! {
@@ -24,11 +30,17 @@ fn main() -> ! {
 
     let mut flash = dp.FLASH.constrain();
     
+    dp.RCC.apb2enr.write(|w| unsafe { w.tim1en().set_bit() }); // enable timer 1 clock 
     let rcc = dp.RCC.constrain();
+    
+
+
     let clocks = rcc.cfgr
         .use_hse(8.MHz())
+        .hclk(72.MHz())
         .freeze(&mut flash.acr);
-    
+
+
     // Create a delay abstraction based on general-pupose 32-bit timer TIM2
 
     //let mut delay = hal::timer::FTimerUs::new(dp.TIM2, &clocks).delay();
@@ -54,22 +66,25 @@ fn main() -> ! {
         gpiob.pb7.into_push_pull_output(&mut gpiob.crl)
     );
     
-    // let mut time_counter = hal::timer::Timer::syst(cp.SYST, &clocks).counter_us();
-    // or
-    let mut time_counter = cp.SYST.counter_us(&clocks);
 
-    _ = time_counter.start(1.secs());
+
+    let mut dwt = cp.DWT;
+    dwt.enable_cycle_counter();
 
     loop {
-        let time = time_counter.now();
 
-        if (time.ticks() / 1_000_000)%2 == 0 {
+        let cycle_counts = hal::pac::DWT::cycle_count();
+        // hprintln!("cycle_counts {}", cycle_counts);
+        let time_us = cycle_counts/(8_000_000/1_000_000);
+        // hprintln!("time {}", time_us);
+        
+
+        if (time_us / 1_000_000)%2 == 0 {
             led.set_high();
         }
         else {
             led.set_low();
         }
-        hprintln!("time {}", time.ticks());
         stepper_driver.step(&mut delay);
         stepper_driver2.step(&mut delay);
         // delay.delay_us(10000_u16);
